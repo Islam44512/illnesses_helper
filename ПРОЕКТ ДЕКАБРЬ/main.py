6ebcd427c24bcda6b2f4e49622197b5bed037b87
Islam44512 <Islam05.06.13@yandex.ru>
2025-12-27 15:13:08 +0500

    Add files via upload

--- ПРОЕКТ ДЕКАБРЬ/main.py
+++ ПРОЕКТ ДЕКАБРЬ/main.py
@@ -1,11 +1,17 @@
 from telebot import TeleBot
 from bs4 import BeautifulSoup
 from config import TOKEN
-
+from parser_copy import screach
 
 bot = TeleBot(TOKEN)
 
-
+@bot.message_handler(commands=["info"])
+def info(message):
+    bot.reply_to(message, "Напишите название болезни")
+    bot.register_next_step_handler(message, pars)
+def pars(message):
+    all_text = screach(message.text)
+    bot.reply_to(message, all_text)
 
 
 
