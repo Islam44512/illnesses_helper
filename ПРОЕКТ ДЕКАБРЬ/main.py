d163c8076c7ddec6ca3242d84879505b2696f70d
Islam44512 <Islam05.06.13@yandex.ru>
2025-12-28 10:44:12 +0500

    Add files via upload

--- ПРОЕКТ ДЕКАБРЬ/main.py
+++ ПРОЕКТ ДЕКАБРЬ/main.py
@@ -2,6 +2,7 @@ from telebot import TeleBot
 from bs4 import BeautifulSoup
 from config import TOKEN
 from parser_copy import screach
+import string
 
 bot = TeleBot(TOKEN)
 
@@ -11,8 +12,14 @@ def info(message):
     bot.register_next_step_handler(message, pars)
 def pars(message):
     all_text = screach(message.text)
-    bot.reply_to(message, all_text)
+    if all_text == "":
+        bot.reply_to(message, "Призошла непривидинная ошибка")
+    else:
+        all_text = " ".join(all_text)
+        for i in range(1,20):
+            all_text = all_text.replace(f"[{i}]", "")
+        bot.reply_to(message, all_text)
 
 
 
-bot.infinity_polling()
+bot.polling()
